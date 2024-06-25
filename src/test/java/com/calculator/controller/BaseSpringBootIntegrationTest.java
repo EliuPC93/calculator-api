1816e4eaeb68c3747f1ac74c1f94f522f9345ec2
EliuPC93 <irvingeliu.93@gmail.com>
2024-06-25 01:14:46 -0600

    using base class for testing

---
+++ src/test/java/com/calculator/controller/BaseSpringBootIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.calculator.controller;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseSpringBootIntegrationTest {
+    @LocalServerPort
+    private int serverPort;
+    protected URI serverUri;
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @Before
+    public void configureIntegrationEnv() throws Exception {
+        serverUri = new URI("http://localhost:" + serverPort + "/v1/users");
+    }
+}
--- src/test/java/com/calculator/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/calculator/controller/UserControllerIntegrationTest.java
@@ -4,40 +4,28 @@ import com.calculator.core.service.UserService;
 import com.calculator.data.request.NewUser;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.mockito.Mockito.*;
 
-@RunWith(SpringRunner.class)
-@ActiveProfiles("test")
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class UserControllerIntegrationTest {
+public class UserControllerIntegrationTest extends BaseSpringBootIntegrationTest {
 
     @Autowired
     private UserController rentalController;
-    @LocalServerPort
-    private int serverPort;
+
     @MockBean
     private UserService userService;
-    @Autowired
-    private TestRestTemplate restTemplate;
+
     @Test
     public void createNewOperation_should_returnHttpStatusOk_when_thereAreNoErrors() throws Exception {
         NewUser expectedRequest = NewUser.builder().username("jorge@mail.com").password("heyou").build();
 
         UserService userService1 = mock(UserService.class);
         doNothing().when(userService1).register(expectedRequest);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://localhost:" + serverPort + "/v1/users", expectedRequest, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(serverUri, expectedRequest, String.class);
 
         Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
